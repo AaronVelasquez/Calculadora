9bf49c465478599bf79668cbeb97a1f839d92f2c
Aaron Velasquez <ajva24@gmail.com>
2024-01-24 14:23:54 -0300

    tercera pre entrega

--- script/main.js
+++ script/main.js
@@ -2,90 +2,130 @@
 let acciones=[]
 let nombre;
 let user;
+let alerta = document.getElementById("alertas");
+
 function sumar() {
     let n1 = parseInt(document.getElementById("n1").value);
     let n2 = parseInt(document.getElementById("n2").value);
-    let n3 = parseInt(document.getElementById("n3").value);
     let resultado = 0;
-    if(isNaN(n1) || isNaN(n2)|| isNaN(n3)){
-        alert("debe ingresar tres números")
+    if (isNaN(n1) || isNaN(n2)) {
+        alerta.style.display= "inline-block";
+        alerta.innerHTML = "Debe ingresar dos números" //remplazo de alert();
+        // alert("debe ingresar dos números")
     }
-    else{
-        resultado = n1 + n2 + n3;
-    alert("el resultado es: " + resultado)
+    else {
+        resultado = n1 + n2;
+        alerta.style.display= "inline-block";
+        alerta.innerHTML = "El resultado es: " + resultado;
     }
-    acciones.push("sumar")
+    if (!sessionStorage.getItem('acciones')) {
+        sessionStorage.setItem('acciones', JSON.stringify([]));
+      }
+      let accionesActual = JSON.parse(sessionStorage.getItem('acciones'));
+      acciones.push("Suma");
+      sessionStorage.setItem('acciones', JSON.stringify(acciones));
 }
-
 function restar() {
     let n1 = parseInt(document.getElementById("n1").value);
     let n2 = parseInt(document.getElementById("n2").value);
-    let n3 = parseInt(document.getElementById("n3").value);
     let resultado = 0;
-    resultado = n1 - n2 - n3;
-    alert("el resultado es: " + resultado)
-    acciones.push("restar")
+    resultado = n1 - n2;
+    alerta.style.display= "inline-block";
+    alerta.innerHTML = "El resultado es: " + resultado;
+    if (!sessionStorage.getItem('acciones')) {
+        sessionStorage.setItem('acciones', JSON.stringify([]));
+      }
+      let accionesActual = JSON.parse(sessionStorage.getItem('acciones'));
+      acciones.push("Resta");
+      sessionStorage.setItem('acciones', JSON.stringify(acciones));
 }
-
 function multiplicar() {
     let n1 = parseInt(document.getElementById("n1").value);
     let n2 = parseInt(document.getElementById("n2").value);
-    let n3 = parseInt(document.getElementById("n3").value);
     let resultado = 0;
-    resultado = n1 * n2 * n3;
-    alert("el resultado es: " + resultado)
-    acciones.push("multiplicar")
+    resultado = n1 * n2;
+    alerta.style.display= "inline-block";
+    alerta.innerHTML = "El resultado es: " + resultado;
+    if (!sessionStorage.getItem('acciones')) {
+        sessionStorage.setItem('acciones', JSON.stringify([]));
+      }
+      let accionesActual = JSON.parse(sessionStorage.getItem('acciones'));
+      acciones.push("Multiplicar");
+      sessionStorage.setItem('acciones', JSON.stringify(acciones));
+ 
 }
-
 function dividir() {
     let n1 = parseInt(document.getElementById("n1").value);
     let n2 = parseInt(document.getElementById("n2").value);
-    let n3 = parseInt(document.getElementById("n3").value);
     let resultado = 0;
-    if (n3 == 0) {
-        alert("imposible dividir entre 0");
+    if (n2 == 0) {
+        alerta.style.display= "inline-block";
+        alerta.innerHTML = "No se puede dividir entre 0";
     }
     else {
-        resultado = n1 / n2 / n3;
-        alert("el resultado es: " + resultado)
+        resultado = n1 / n2;
+        alerta.style.display= "inline-block";
+        alerta.innerHTML = "El resultado es: " + resultado;
+        if (!sessionStorage.getItem('acciones')) {
+            sessionStorage.setItem('acciones', JSON.stringify([]));
+          }
+          let accionesActual = JSON.parse(sessionStorage.getItem('acciones'));
+          acciones.push("Dividir");
+          sessionStorage.setItem('acciones', JSON.stringify(acciones));
+    }
  
-    }
-    acciones.push("dividir")
 }
-
-function mensaje(){
-    let n4=prompt("Digite un número mayor a 0");
-    let result=parseInt(n4);
-    if(isNaN(result)){
-        alert("Ingrese un número")
-    }
-    else{
-        let resultado="";
-    for(let i=1;i<11;i++){
-        resultado += `${result} * ${i} = ${result * i}\n`;
-    }
-    alert(resultado);
+function mensaje() {
+    let input = `
+    <form method = "post">
+    <input type="number" placeholder="ingresa un numero" id="n4">
+    <button type="button" onclick="ejecutar()">enviar</button>
+    </form>`;
  
-    }
+    alerta.style.display= "inline-block";
+    alerta.innerHTML = input;
+   
 }
-
+ 
+function ejecutar(){
+    let result = parseInt(document.getElementById("n4").value);
+        let resultado = "";
+        for (let i = 1; i < 11; i++) {
+            resultado += `${result} * ${i} = ${result * i}`+"</br>";        
+        }
+        alerta.innerHTML = resultado;
+}
+ 
 function moves() {
     let result = "<table>";
-    for (const element of acciones) {
-        result += "<tr><td>" + element + "</td></tr>";
-      }
+    let lista = sessionStorage.getItem("acciones".split(","));
+    let newlist = JSON.parse(lista);
+    let getid = document.getElementById("show-acciones");
+ 
+     for (let i = 0; i< newlist.length; i++) {
+        result += "<tr><td>" + newlist[i] + "</td></tr>";
+     }
+ 
       result += "</table>";
-    let getid=document.getElementById("show-acciones")
-    getid.innerHTML=result
+      getid.innerHTML = result;
 }
-
-function search(){    
-    let dato=document.getElementById("buscar").value.toLowerCase();
-    let items = acciones.filter(accion => accion.toLowerCase() === dato);
-    let cantidad=items.length
-    alert("La operación " + dato +" se ha realizado " + cantidad + " veces");
+ 
+function search() {
+    let lista = sessionStorage.getItem("acciones".split(","));
+    let newlist = JSON.parse(lista);
+    let selectElement = document.getElementById('seleccion');
+    let selectedValue = selectElement.value;
+    let getid = document.getElementById("show-acciones");
+    let contador=0;
+   
+    for(let i =0 ; i<newlist.length; i++){
+        if(newlist[i].toLowerCase() == selectedValue){
+            contador++;
+    }  
+    getid.innerHTML = "La acción " + selectedValue + " se ha realizado " + contador + " veces";
+ }
+ 
 }
-
 
 function perfil(){
    nombre= prompt("ingrese su nombre");
@@ -99,4 +139,4 @@ function info(){
     }  
     alert("hola "+object.name + " tu nick es: "+ object.nick);  
 }
-
+    
